c6290bda2c4e1b628198f9adcd0e9f6c5bf1cd55
souljazzfunk <59495284+souljazzfunk@users.noreply.github.com>
2023-09-13 16:32:18 +0900

    add CSS and edit function

--- content.js
+++ content.js
@@ -1,12 +1,30 @@
-chrome.storage.local.get(null, function(items) {
-  const forms = document.querySelectorAll('.cmn-column.project-list__item');
-
-  forms.forEach(function(form) {
-    const action = form.getAttribute('action');
-    const urlId = action.split('/').pop(); // Assuming the ID is the last part of the URL
-
-    if (items[urlId] === 'applied') {
-      form.style.backgroundColor = '#dbeafe'; // Change to your desired color
-    }
+// Fetch all saved job IDs from Chrome's local storage
+chrome.storage.local.get(null)
+  .then(items => {
+    // Query the DOM to find all job list items
+    const forms = document.querySelectorAll('.cmn-column.project-list__item');
+    
+    // Iterate through each form to apply custom styling for applied jobs
+    forms.forEach(form => applyCustomStyling(form, items));
+  })
+  .catch(err => {
+    console.error('An error occurred while fetching saved job IDs:', err);
   });
-});
+
+/**
+ * Applies custom styling to a job list item based on its application status.
+ * 
+ * @param {HTMLElement} form - The form element representing a job list item.
+ * @param {Object} items - An object containing saved job data.
+ */
+const applyCustomStyling = (form, items) => {
+  // Extract the last part of the URL from the form action attribute as the unique job ID
+  const action = form.getAttribute('action');
+  const urlId = action.split('/').pop();
+  
+  // Check if this job has been marked as applied
+  if (items[urlId] && items[urlId].status === 'applied') {
+    // Apply custom styling for jobs that are marked as applied
+    form.style.backgroundColor = '#dbeafe';
+  }
+};
--- popup.js
+++ popup.js
@@ -1,16 +1,92 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const markAsAppliedButton = document.getElementById('markAsApplied');
+document.addEventListener('DOMContentLoaded', () => {
+  const markAsAppliedButton = document.getElementById('markAsApplied')
+  const jobListContainer = document.getElementById('jobListContainer')
 
-  markAsAppliedButton.addEventListener('click', function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      const currentTab = tabs[0];
-      const url = currentTab.url;
-      const urlId = url.split('/').pop(); // Assuming the ID is the last part of the URL
+  markAsAppliedButton.addEventListener('click', markJobAsApplied)
+  displaySavedJobs(jobListContainer)
 
-      // Save to local storage
-      chrome.storage.local.set({ [urlId]: 'applied' }, function() {
-        alert('Job marked as applied.');
-      });
+  validateCurrentPage()
+})
+
+const markJobAsApplied = async () => {
+  try {
+    const [{ url, title }] = await chrome.tabs.query({ active: true, currentWindow: true })
+    const urlId = url.split('/').pop()
+    const timestamp = new Date().toISOString()
+
+    const jobData = {
+      status: 'applied',
+      timestamp,
+      title
+    }
+
+    await chrome.storage.local.set({ [urlId]: jobData })
+    displaySavedJobs(document.getElementById('jobListContainer'))
+  } catch (error) {
+    console.error('An error occurred:', error)
+  }
+}
+
+function timeAgo(timestamp) {
+  const now = new Date()
+  const date = new Date(timestamp)
+  const diffInSeconds = Math.floor((now - date) / 1000)
+
+  let timeAgo = ''
+  if (diffInSeconds < 60) return `${diffInSeconds} seconds ago`
+  const minutes = Math.floor(diffInSeconds / 60)
+  if (minutes < 60) return `${minutes} minutes ago`
+  const hours = Math.floor(minutes / 60)
+  if (hours < 24) return `${hours} hours ago`
+  const days = Math.floor(hours / 24)
+  if (days < 30) return `${days} days ago`
+  const months = Math.floor(days / 30)
+  if (months < 12) return `${months} months ago`
+  const years = Math.floor(months / 12)
+  return `${years} years ago`
+}
+
+const displaySavedJobs = async (container) => {
+  try {
+    const items = await chrome.storage.local.get(null)
+    container.innerHTML = ''
+
+    Object.entries(items).forEach(([urlId, jobData]) => {
+      if (jobData.status === 'applied') {
+        const listItem = document.createElement('li')
+        listItem.textContent = `${jobData.title} - ${timeAgo(jobData.timestamp)}`
+
+        const deleteButton = document.createElement('button')
+        deleteButton.textContent = 'Delete'
+        deleteButton.addEventListener('click', () => editJob(urlId))
+
+        listItem.append(deleteButton)
+        container.append(listItem)
+      }
+    })
+  } catch (error) {
+    console.error('An error occurred while fetching saved jobs:', error)
+  }
+}
+
+const editJob = (urlId) => {
+  const isConfirmed = confirm('Are you sure you want to delete this job entry?');
+
+  if (isConfirmed) {
+    chrome.storage.local.remove(urlId, () => {
+      displaySavedJobs(document.getElementById('jobListContainer'));
     });
-  });
-});
+  }
+};
+
+
+const validateCurrentPage = async () => {
+  const [{ url }] = await chrome.tabs.query({ active: true, currentWindow: true })
+  const jobDescriptionPattern = /https:\/\/lancersagent\.com\/project\/engineer\/\d+/
+  
+  if (!jobDescriptionPattern.test(url)) {
+    const markAsAppliedButton = document.getElementById('markAsApplied')
+    markAsAppliedButton.disabled = true
+    markAsAppliedButton.textContent = "Not a job description page"
+  }
+}
